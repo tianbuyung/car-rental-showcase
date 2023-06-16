49e157520d2af01d243f20f2ad3625e4287cab24
Septian Maulana <septianm028@gmail.com>
2023-06-16 10:48:33 +0700

    feat: create search bar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,13 @@ const Hero = () => {
 
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <Image
+            src="/hero.png"
+            alt="hero"
+            fill
+            className="object-contain"
+            sizes="100vw"
+          />
         </div>
 
         <div className="hero__image-overlay" />
