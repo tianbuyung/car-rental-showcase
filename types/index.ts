37cf6ced8b5c9f7d1877a3132a3efd215e6997c5
Septian Maulana <septianm028@gmail.com>
2023-06-21 10:31:24 +0700

    fix: fixed bugs

--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,5 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps {
-  title: string;
-  containerStyles?: string;
-  handleClick?: MouseEventHandler<HTMLButtonElement>;
-  btnType?: "button" | "submit";
-  textStyles?: string;
-  rightIcon?: string;
-  isDisabled?: boolean;
-}
-
-// export interface SearchManufacturerProps {
-//   manufacturer: string;
-//   setManufacturer: (manufacturer: string) => void;
-// }
-
 export interface CarProps {
   city_mpg: number;
   class: string;
@@ -30,33 +15,6 @@ export interface CarProps {
   year: number;
 }
 
-export interface FilterProps {
-  manufacturer: string;
-  year: number;
-  fuel: string;
-  limit: number;
-  model: string;
-}
-
-export interface OptionProps {
-  title: string;
-  value: string;
-}
-
-// export interface CustomFilterProps {
-//   title: string;
-//   options: OptionProps[];
-// }
-
-// export interface ShowMoreProps {
-//   pageNumber: number;
-//   isNext: boolean;
-// }
-
-export interface HomeProps {
-  searchParams: FilterProps;
-}
-
 export type CarState = CarProps[] & { message?: string };
 
 export interface SearchBarProps {
@@ -64,9 +22,37 @@ export interface SearchBarProps {
   setModel: (model: string) => void;
 }
 
-export interface SearchManuFacturerProps {
-  selected: string;
-  setSelected: (selected: string) => void;
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
+export interface CarCardProps {
+  model: string;
+  make: string;
+  mpg: number;
+  transmission: string;
+  year: number;
+  drive: string;
+  cityMPG: number;
+}
+
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
 }
 
 export interface CustomFilterProps<T> {
@@ -78,4 +64,9 @@ export interface ShowMoreProps {
   pageNumber: number;
   isNext: boolean;
   setLimit: (limit: number) => void;
+}
+
+export interface SearchManuFacturerProps {
+  selected: string;
+  setSelected: (selected: string) => void;
 }
